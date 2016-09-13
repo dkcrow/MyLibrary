e98bcd243e44e27650b762b2671f484dd9f30571
dark_crow <dark_crow@foxmail.com>
2016-09-13 12:29:01 +0800

    再次测试

---
+++ MyLibrary/DemoManager/MyTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.DemoManager
+{
+    public class MyTest
+    {
+         public MyTest()
+        {
+            Console.WriteLine("my test now");
+        }
+    }
+}
